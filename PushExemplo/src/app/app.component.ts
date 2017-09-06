c15b15db98069d7386c1688837c133d325b1e1c6
Anne Kelly <annekelly533@gmail.com>
2017-09-06 09:18:42 -0300

    Push Notifications OneSignal + Firebase

---
+++ PushExemplo/src/app/app.component.ts
@@ -0,0 +1,39 @@
+import { OneSignal } from '@ionic-native/onesignal';
+import { Component } from '@angular/core';
+import { Platform } from 'ionic-angular';
+import { StatusBar } from '@ionic-native/status-bar';
+import { SplashScreen } from '@ionic-native/splash-screen';
+
+import { HomePage } from '../pages/home/home';
+@Component({
+  templateUrl: 'app.html'
+})
+export class MyApp {
+  rootPage:any = HomePage;
+
+  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen, public oneSignal : OneSignal ) {
+    platform.ready().then(() => {
+      // Okay, so the platform is ready and our plugins are available.
+      // Here you can do any higher level native things you might need.
+      statusBar.styleDefault();
+      splashScreen.hide();
+
+      //Aqui vocë coloca os dados que coletamos no passo 12 e 7
+      this.oneSignal.startInit("883e105d-e660-45c8-84d1-878064992b27", "267519198814");
+
+      //Aqui é caso vocë queria que o push apareça mesmo com o APP aberto
+      this.oneSignal.inFocusDisplaying(this.oneSignal.OSInFocusDisplayOption.Notification);
+
+      //Aqui você vai tratar o recebimento do push notification com todos os dados
+      this.oneSignal.handleNotificationOpened().subscribe(data => {
+
+        console.log("Dados do Push", data);
+
+      });
+
+      this.oneSignal.endInit();
+
+    });
+  }
+}
+
